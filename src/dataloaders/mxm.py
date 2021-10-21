a3237abca04097a9187e25f53d24a7e50fe0721c
Max Paymar <wmpaymar@gmail.com>
2021-10-21 19:55:20 -0400

    add initial dataset code

--- src/dataloaders/mxm.py
+++ src/dataloaders/mxm.py
@@ -1,11 +1,37 @@
-from torchtext.vocab import build_vocab_from_iterator
+from torch.utils.data import Dataset
+import pandas as pd
 
-# https://pytorch.org/tutorials/beginner/text_sentiment_ngrams_tutorial.html
+class MusixMatchDataset(Dataset):
+    def __init__(self, filepath):
+        with open(filepath) as inputfile:
+            for i, line in enumerate(inputfile):
+                if line[0] is '#':
+                    continue
+                elif line[0] is '%':
+                    self.__classes = self.__parse_word_list__(line)
+                else:
+                    skip_rows = i + 1
+                    break
+        self.data = pd.read_csv(filepath, skip_rows=skip_rows)
 
-mxm_tokenizer = lambda line: line.split(',')
+    def __len__(self):
+        return len(self.data)
 
-def yield_tokens(data_iter):
-    pass
+    def __get_item__(self, index):
+        line = self.data.iloc[index, 1:]
+        item = torch.zeros(len(self.__classes))
+        for token in line.split(','):
+            song_index, count = token.split(':')
+            item[song_index] = count
+        return item
 
-with open('./files/mxm_dataset_train.txt') as f:
-    pass
+    @staticmethod
+    def __parse_word_list__(line):
+        return line[1:].split(',')
+
+    def get_classes():
+        return self.__classes
+
+        
+
+

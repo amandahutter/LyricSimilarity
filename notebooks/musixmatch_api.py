b6606cef37cc49c7d2e5cad52bf311836fa21a24
Max Paymar <wmpaymar@gmail.com>
2021-10-25 21:16:38 -0400

    working example

--- notebooks/musixmatch_api.py
+++ notebooks/musixmatch_api.py
@@ -7,8 +7,10 @@ swagger_client.configuration.api_key['apikey'] = os.getenv('MXM_API_KEY')
 # create an instance of the API class
 api_instance = swagger_client.TrackApi()
 
+print(dir(api_instance))
+
 try:
-    req = api_instance.track_get_get(6447080)
+    req = api_instance.track_get_get(5920049)
     print(dir(req))
     print(req)
 except ApiException as api_ex:

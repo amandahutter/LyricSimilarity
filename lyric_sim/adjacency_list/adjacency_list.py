0b728e1a4aaa96b62889cce14bea27f809d66fa1
Max Paymar <wmpaymar@gmail.com>
2021-11-24 18:47:37 -0500

    add module for getting similarity scores (in memory)

---
+++ lyric_sim/adjacency_list/adjacency_list.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+class SongNotFoundException(Exception):
+    def __init__(self, mxm_id):
+        super().__init__(f'Song with mxm id {mxm_id} not found in adjacency list')
+
+class AdjacencyList:
+    def __init__(self, similarity_database):
+        self.__adjacency_list = {}
+        con = sqlite3.connect(similarity_database)
+        cur = con.cursor()
+        cur.execute('SELECT * FROM similars_src')
+        rows = cur.fetchall()
+        for i, row in enumerate(rows):
+            if i % 1000 is 0:
+                # Writes these logs on one line
+                print('\r' + f'Loaded {i} song similarities', end="")
+            key = row[0]
+            adjacencies = {}
+            tokens = row[1].split(',')
+            for j in range(0, len(tokens), 2):
+                adjacencies[tokens[j]] = float(tokens[j+1])
+            self.__adjacency_list[key] = adjacencies
+        print('\r' + f'Loaded {i} song similarities')
+    
+    def get_similarity(self, src, dest):
+        if src not in self.__adjacency_list:
+            raise SongNotFoundException(src)
+        try:
+            return self.__adjacency_list[src][dest]
+        except KeyError:
+            return 0
+
+            
+            
+            
---
+++ lyric_sim/adjacency_list/test_adjacency_list.py
@@ -0,0 +1,26 @@
+import unittest
+from lyric_sim.adjacency_list.adjacency_list import AdjacencyList, SongNotFoundException
+
+adjacency_list = AdjacencyList('./data_files/lastfm_similars.db')
+
+class TestAdjacencyList(unittest.TestCase):
+
+    def test_get_adjacency(self):
+        src_id = 'TRCCCYE12903CFF0E9'
+        dest_id = 'TRHZRQH128F92F9AC2'
+        similarity = adjacency_list.get_similarity(src_id, dest_id)
+        self.assertEqual(similarity, 0.498053)
+    
+    def test_song_not_found(self):
+        try:
+            adjacency_list.get_similarity('not_an_id', 'unused')
+        except SongNotFoundException as ex:
+           self.assertEqual(str(ex), 'Song with mxm id not_an_id not found in adjacency list')
+    
+    def test_not_adjecent_returns_zero(self):
+        src_id = 'TRCCCYE12903CFF0E9'
+        similarity = adjacency_list.get_similarity(src_id, 'not_an_id')
+        self.assertEqual(similarity, 0)
+
+if __name__ == '__main__':
+    unittest.main()

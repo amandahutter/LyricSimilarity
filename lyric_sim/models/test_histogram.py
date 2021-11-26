8f39e0afa5a118dfb7a15d911161683380c3f893
Max Paymar <wmpaymar@gmail.com>
2021-11-26 15:22:55 -0500

    fix broken test

--- lyric_sim/models/test_histogram.py
+++ lyric_sim/models/test_histogram.py
@@ -1,14 +1,14 @@
 import unittest
-from lyric_sim.datasets import MusixMatchDataset
+from lyric_sim.datasets.mxm import MusixMatchSqliteDataset
 from lyric_sim.models.histogram import HistogramModel
 import torch
 
-dataset = MusixMatchDataset('./test_files/mxm_dataset_test.txt')
+dataset = MusixMatchSqliteDataset('./test_files/mxm_dataset_test.txt', '', False)
 class TestMxmHistogram(unittest.TestCase):
 
     def test_forward(self):
-        src, label = dataset.__getitem__(0)
-        dest, label = dataset.__getitem__(1)
+        src, _ = dataset.__getitem__(0)
+        dest, _ = dataset.__getitem__(1)
         
         # convert inputs into batches of size N=1
         src = torch.unsqueeze(src, 0)

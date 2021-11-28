ead607c50f462a5867b162c8da016032880024ed
Max Paymar <wmpaymar@gmail.com>
2021-11-28 18:16:10 -0500

    updated code to only use similar pairs

--- lyric_sim/datasets/mxm.py
+++ lyric_sim/datasets/mxm.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch.utils.data import Dataset
 import re
@@ -5,7 +6,106 @@ import numpy as np
 import sqlite3
 from typing import List
 
+NUM_WORDS = 5000
+
+class MxMLastfmJoinedDataset(Dataset):
+    """
+    Constructs a series of examples [1x10000] where the first 5000 words are the word counts for song 1, and the second 5000 are word counds for song two.
+    The labels are similarity score threshold between the two songs is_similar(s1, s2) >= .5 ? 1 : 0. Uses the similars_src table from lastfm only.
+    """
+
+    def __init__(self, mxm_db_path: str, lastfm_db_path: str, use_test: bool, use_saved_data: bool):
+
+        use_test = int(use_test)
+
+        con = sqlite3.connect(mxm_db_path)
+        cur = con.cursor()
+        cur.execute('ATTACH DATABASE ? AS lastfm_sim', (lastfm_db_path,))
+
+        # TODO: add logic for train and test
+        if use_saved_data and os.path.exists('./data_files/histogram_examples.npy') and os.path.exists('./data_files/histogram_labels.npy'):
+            self.__data = np.load('./data_files/histogram_examples.npy')
+            self.__labels = np.load('./data_files/histogram_labels.npy')
+            return
+
+        con = sqlite3.connect(mxm_db_path)
+        cur = con.cursor()
+        cur.execute('ATTACH DATABASE ? AS lastfm_sim', (lastfm_db_path,))
+
+        word_tups = cur.execute('SELECT * FROM words;').fetchall()
+        self.__word_idxs = {}
+        for i, word_tup in enumerate(word_tups):
+            self.__word_idxs[word_tup[0]] = i
+
+        print('Fetching data...')
+
+        self.__data = np.empty([])
+        self.__labels = np.empty([])
+
+        # create each example one by one
+        track_ids = cur.execute("""
+            SELECT DISTINCT(lyrics.track_id)
+            FROM lyrics
+            INNER JOIN lastfm_sim.similars_src
+            ON lyrics.track_id = lastfm_sim.similars_src.tid
+            WHERE lyrics.is_test = ?
+        """, (use_test,)).fetchall()
+
+        for i, track_id_tup in enumerate(track_ids):
+            track_id = track_id_tup[0]
+            print(f'Creating example for track {track_id}.', end="")
+            words = cur.execute("""
+                SELECT lyrics.word, lyrics.count
+                FROM lyrics
+                WHERE lyrics.is_test = ? AND
+                lyrics.track_id = ?;
+            """, (use_test, str(track_id),)).fetchall()
+            
+            example = np.zeros((NUM_WORDS*2))
+            for word, count in words:
+                example[self.__word_idxs[word]] = count
+
+            similars = cur.execute("""
+                SELECT lastfm_sim.similars_src.target
+                FROM lastfm_sim.similars_src
+                WHERE tid = ?
+            """, (track_id,)).fetchone()
+
+            similars_tokens = similars[0].split(',')
+
+            example_copy = np.copy(example)
+
+            print(f'Getting {len(similars_tokens)/2} similars for track {track_id}')
+            for i in range(0, len(similars_tokens), 2):
+                similar_id = similars_tokens[i]
+                similar_score = 1 if float(similars_tokens[i+1]) > .5 else 0
+                similar_words = cur.execute("""
+                    SELECT lyrics.word, lyrics.count
+                    FROM lyrics
+                    WHERE lyrics.track_id = ?;
+                """, (similar_id,)).fetchall()
+
+                for similar in similar_words:
+                    word, count = similar
+                    example[self.__word_idxs[word]+NUM_WORDS] = count
+                
+                self.__data = np.append(self.__data, example_copy)
+                self.__labels = np.append(self.__labels, similar_score)
+
+                np.save('./data_files/histogram_examples.npy', self.__data)
+                np.save('./data_files/histogram_labels.npy', self.__labels)
+
+        def __len__(self):
+            return len(self.__data)
+
+        def __getitem__(self, index):
+            return (torch.Tensor(self.__data[index]), self.__labels[index])
+                    
+
 class MusixMatchSqliteDataset(Dataset):
+    """
+    Deprecated. Filtered the musix match dataset depending on whether the id could be found in the last fm dataset.
+    """
     def __init__(self, mxm_db_path: str, lastfm_db_path: str, filter_by_last_fm: bool, use_test: bool):
         use_test = int(use_test)
         con = sqlite3.connect(mxm_db_path)
@@ -83,6 +183,10 @@ class MusixMatchSqliteDataset(Dataset):
         return (torch.Tensor(self.data[index]), self.__idx_to_track[index])
 
 class MusixMatchCsvDataset(Dataset):
+    """
+    Exposes the musix match word count data as a list of tensors 1x5000 where the 5000 columns are each attributed to a word in the words list.
+    e.g. [4, 2, 0, 0, 3, 1, ... , 0, 0, 1, 0, 0]
+    """
     def __init__(self, filepath):
         self.__idx_to_mxmid = {}
         with open(filepath) as inputfile:
--- lyric_sim/datasets/test_mxm.py
+++ lyric_sim/datasets/test_mxm.py
@@ -1,41 +1,63 @@
 import unittest
-from lyric_sim.datasets.mxm import MusixMatchCsvDataset, MusixMatchSqliteDataset
+from lyric_sim.datasets.mxm import MusixMatchCsvDataset, MusixMatchSqliteDataset, MxMLastfmJoinedDataset
 
-csv_dataset = MusixMatchCsvDataset('./test_files/mxm_dataset.txt')
-sqlite_dataset = MusixMatchSqliteDataset('./test_files/mxm_dataset.db', './test_files/lastfm_similars.db', False, False)
-
-class TestMxmSqliteDataset(unittest.TestCase):
-
-    def test_num_words(self):
-        self.assertEqual(len(sqlite_dataset.get_words()), 5000)
+class TestMxMLastfmJoinedDataset(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        super(TestMxMLastfmJoinedDataset, self).setUpClass()
+        self.dataset = MxMLastfmJoinedDataset('./data_files/mxm_dataset.db', './data_files/lastfm_similars.db', False, False)
 
     def test_getitem(self):
-        input, label = sqlite_dataset.__getitem__(0)
+        input, label = self.dataset.__getitem__(0)
+        self.assertEqual(input.shape[0], 10000)
+        self.assertEqual(label, 'TRAAAAV128F421A322')
+    
+    def test_getlen(self):
+        self.assertEqual(self.dataset.__len__(), 23)
+
+@unittest.skip("deprecated")
+class TestMxmSqliteDataset(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        super(TestMxmSqliteDataset, self).setUpClass()
+        self.sqlite_dataset = MusixMatchSqliteDataset('./test_files/mxm_dataset.db', './test_files/lastfm_similars.db', False, False)
+
+    def test_num_words(self):
+        self.assertEqual(len(self.sqlite_dataset.get_words()), 5000)
+
+    def test_getitem(self):
+        input, label = self.sqlite_dataset.__getitem__(0)
         self.assertEqual(input.shape[0], 5000)
         self.assertEqual(label, 'TRAAAAV128F421A322')
     
     def test_getlen(self):
-        self.assertEqual(sqlite_dataset.__len__(), 23)
+        self.assertEqual(self.sqlite_dataset.__len__(), 23)
     
     def test_lyric_index(self):
-        input, _ = sqlite_dataset.__getitem__(0)
+        input, _ = self.sqlite_dataset.__getitem__(0)
         self.assertEqual(input[0], 6)
 
 class TestMxmCsvDataset(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(self):
+        super(TestMxmCsvDataset, self).setUpClass()
+        self.csv_dataset = MusixMatchCsvDataset('./test_files/mxm_dataset.txt')
+
     def test_num_words(self):
-        self.assertEqual(len(csv_dataset.get_words()), 5000)
+        self.assertEqual(len(self.csv_dataset.get_words()), 5000)
 
     def test_getitem(self):
-        input, label = csv_dataset.__getitem__(0)
+        input, label = self.csv_dataset.__getitem__(0)
         self.assertEqual(input.shape[0], 5000)
         self.assertEqual(label, 'TRAABRX12903CC4816')
     
     def test_getlen(self):
-        self.assertEqual(csv_dataset.__len__(), 128)
+        self.assertEqual(self.csv_dataset.__len__(), 128)
     
     def test_lyric_index(self):
-        input, _ = csv_dataset.__getitem__(0)
+        input, _ = self.csv_dataset.__getitem__(0)
         self.assertEqual(input[1], 19)
 
 

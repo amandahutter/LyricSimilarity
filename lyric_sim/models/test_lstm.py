de454c0d374ebaddc05f1f29626d5bc7afdc2ae5
Max Paymar <wmpaymar@gmail.com>
2021-12-02 22:01:32 -0500

    fix enum

--- lyric_sim/models/test_lstm.py
+++ lyric_sim/models/test_lstm.py
@@ -1,5 +1,5 @@
 import unittest 
-from lyric_sim.models.lstm import LSTM
+from lyric_sim.models.lstm import LSTM, CombinationType
 import torch 
 import numpy as np 
 
@@ -14,19 +14,19 @@ class TestLyricsLSTM(unittest.TestCase):
         song2 = torch.randint(low = 0, high = vocab_size, size = (1,20))
         similar = 1 
 
-        model = LSTM(vocab_size, 20, 10, 0.9, 1, 'ADD') 
+        model = LSTM(vocab_size, 20, 10, 0.9, 1, CombinationType.ADD) 
         y = model.forward(song1, song2)
         self.assertEqual(len(y), 1)
 
-        model = LSTM(vocab_size, 20, 10, 0.9, 1, 'SUB') 
+        model = LSTM(vocab_size, 20, 10, 0.9, 1, CombinationType.SUB) 
         y = model.forward(song1, song2)
         self.assertEqual(len(y), 1)
 
-        model = LSTM(vocab_size, 20, 10, 0.9, 1, 'MULT') 
+        model = LSTM(vocab_size, 20, 10, 0.9, 1, CombinationType.MULT) 
         y = model.forward(song1, song2)
         self.assertEqual(len(y), 1)
 
-        model = LSTM(vocab_size, 20, 10, 0.9, 1, 'ALL') 
+        model = LSTM(vocab_size, 20, 10, 0.9, 1, CombinationType.ALL) 
         y = model.forward(song1, song2)
         self.assertEqual(len(y), 1)
 
